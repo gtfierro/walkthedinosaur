0fb4c7a343b887ff4dc085f62a934a75fb03b9b2
Gabe Fierro <gtfierro225@gmail.com>
2013-09-19 14:28:26 -0700

    add filter to convert tables to json

--- batchsql/templatetags/batchsql_filters.py
+++ batchsql/templatetags/batchsql_filters.py
@@ -1,4 +1,5 @@
 from django import template
+import json
 
 register = template.Library()
 
@@ -6,3 +7,9 @@ register = template.Library()
 def get_item(d, key):
     return d.get(key)
 
+@register.filter
+def to_json(tables):
+    d = {}
+    for key, value in tables.iteritems():
+        d[key] = [str(c.name) for c in value]
+    return json.dumps(d)
