46df2a214d9c66d740c324d0707f1ac8c4eedd8f
Gabe Fierro <gtfierro225@gmail.com>
2013-09-19 14:27:30 -0700

    configure static paths

--- walkthedinosaur/settings.py
+++ walkthedinosaur/settings.py
@@ -1,5 +1,9 @@
 # Django settings for walkthedinosaur project.
 
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -69,6 +73,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
