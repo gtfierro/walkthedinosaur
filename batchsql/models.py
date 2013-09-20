be9c728355e9b22c2df6c1bd07ccd824ec55a70b
Gabe Fierro <gtfierro225@gmail.com>
2013-09-19 18:59:46 -0700

    expand schema to accept arbitrary parameters and create CompletedJobs

--- batchsql/models.py
+++ batchsql/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from uuid import uuid1
 
 CSV = 'CSV'
 TSV = 'TSV'
@@ -11,6 +12,7 @@ FORMAT_CHOICES = (
 )
 
 class QueuedJob(models.Model):
+    id = models.CharField(max_length=50, primary_key=True, default=str(uuid1()))
     date_submitted = models.DateTimeField(default=datetime.now())
     query_string = models.TextField()
     requested_format = models.CharField(max_length=3,
@@ -19,17 +21,30 @@ class QueuedJob(models.Model):
     destination_email = models.CharField(max_length=50)
 
     @classmethod
-    def create(klass, tablename, fields):
+    def create(klass, tablename, fields, requested_format, destination_email):
         querystring = "select {0} from {1};".format(','.join(fields), tablename)
         job = QueuedJob(query_string = querystring,
-                        destination_email = 'a@a.com')
+                        requested_format = requested_format,
+                        destination_email = destination_email)
         return job
 
 class CompletedJob(models.Model):
+    old_jobid = models.CharField(max_length=50)
     date_submitted = models.DateTimeField()
-    date_completed = models.DateTimeField()
+    date_completed = models.DateTimeField(default=datetime.now())
     query_string = models.TextField()
     requested_format = models.CharField(max_length=3,
                                         choices=FORMAT_CHOICES,
                                         default=CSV)
     destination_email = models.CharField(max_length=50)
+    result_filename = models.CharField(max_length=50)
+
+    @classmethod
+    def create(klass, qj, result_filename):
+        job = CompletedJob(query_string = qj.query_string,
+                           requested_format = qj.requested_format,
+                           destination_email = qj.destination_email,
+                           date_submitted = qj.date_submitted,
+                           old_jobid = qj.id,
+                           result_filename = result_filename)
+        return job
