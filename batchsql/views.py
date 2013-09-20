412744104bb1c1df1cfbe04ea7039a2686624e33
Gabe Fierro <gtfierro225@gmail.com>
2013-09-19 19:00:41 -0700

    supply required context for front page

--- batchsql/views.py
+++ batchsql/views.py
@@ -2,13 +2,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
-from batchsql.models import QueuedJob
+from batchsql.models import QueuedJob, CompletedJob
 import connection
 tables = connection.tables
 
 def index(request):
     number_of_queries = len(QueuedJob.objects.all())
-    context = {'number_of_queries': number_of_queries}
+    number_of_finished = len(CompletedJob.objects.all())
+    context = {'number_of_queries': number_of_queries,
+               'number_of_finished': number_of_finished}
     return render(request, 'batchsql/index.html', context)
 
 def define_query(request):
