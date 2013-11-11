836d8b626c77c6792d2d16ea3448cc5f90461292
Aditya Kaulagi <adityakaulagi@gmail.com>
2013-11-11 04:17:49 -0800

    Removed setup_environ as its deprecated.

--- run_jobs.py
+++ run_jobs.py
@@ -1,6 +1,8 @@
-from walkthedinosaur import settings
 from django.core.management import setup_environ
-setup_environ(settings)
+from django.conf import settings
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'walkthedinosaur.settings'
 
 from batchsql import models
 from django.core.mail import send_mail
