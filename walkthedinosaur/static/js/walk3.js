a146801a2338abc522d63d742527a5efc94065d1
Aditya Kaulagi <adityakaulagi@gmail.com>
2014-01-10 17:28:25 +0300

    Added email validation

--- walkthedinosaur/static/js/walk3.js
+++ walkthedinosaur/static/js/walk3.js
@@ -1,24 +1,26 @@
-$('#search-form').validate({
-    debug: true,
-    rules: {
-        email: {
-            email: true,
-            required: true
-        }
-    },
-    highlight: function(element) {
-        $(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function(element) {
-        $(element).closest('.form-group').removeClass('has-error');
-    },
-    errorElement: 'span',
-    errorClass: 'help-block',
-    errorPlacement: function(error, element) {
-        if(element.parent('.input-group').length) {
-            error.insertAfter(element.parent());
-        } else {
-            error.insertAfter(element);
+window.onload=function() {
+    $('#search-form').validate({
+	debug: true,
+	rules: {
+            email: {
+		email: true,
+		required: true
             }
-    }
-});
+	},
+	highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+	},
+	unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+	},
+	errorElement: 'span',
+	errorClass: 'help-block',
+	errorPlacement: function(error, element) {
+            if(element.parent('.input-group').length) {
+		error.insertAfter(element.parent());
+            } else {
+		error.insertAfter(element);
+            }
+	}
+    });
+}
