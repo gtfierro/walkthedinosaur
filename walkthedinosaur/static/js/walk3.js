2143e334ab85d5636e8788839c3724eae73c7d07
Aditya Kaulagi <adityakaulagi@gmail.com>
2014-01-10 16:13:14 +0300

    Separating JS for form validation

---
+++ walkthedinosaur/static/js/walk3.js
@@ -0,0 +1,24 @@
+$('#search-form').validate({
+    debug: true,
+    rules: {
+        email: {
+            email: true,
+            required: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+            }
+    }
+});
